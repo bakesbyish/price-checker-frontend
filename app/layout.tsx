281430f5d5c9ced4a52e0f62631b0159891b4f17
bakesbyish <ishani@bakesbyish.com>
2023-10-30 23:16:36 +0530

    Added the meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,37 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Inter as FontSans } from "next/font/google"
-import { cn } from '~/lib/utils'
-import Providers from './provider'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "~/lib/utils";
+import Providers from "./provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Find products',
-  description: 'Find products by name, SKU, price or cost',
-}
+  title: "Find products",
+  description: "Find products by name, SKU, price or cost",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-      <body className={cn(
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>
+          fontSans.variable,
+        )}
+      >
         <Providers>
           {children}
         </Providers>
       </body>
     </html>
-  )
+  );
 }
