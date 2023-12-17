ea8fc32ad40cdbbfdfcdbe03ece1f77473da6bf1
bakesbyish <ishani@bakesbyish.com>
2023-12-17 21:48:52 +0530

    Added some api keys

--- modules.d.ts
+++ modules.d.ts
@@ -6,7 +6,9 @@ declare namespace NodeJS {
     ALGOLIA_WRITE_KEY: string;
     ALGOLIA_INDEX_NAME: string;
     DOMAIN: string;
-    POS_USERNAME: string;
-    POS_PASSWORD: string;
+    POS_API_KEY_1: string;
+    POS_API_KEY_2: string;
+    POS_API_KEY_3: string;
+    POS_API_KEY_4: string;
   }
 }
