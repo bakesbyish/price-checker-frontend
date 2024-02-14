020794421324be63ea104ed02e2f87bbcf7c13c6
bakesbyish <ishani@bakesbyish.com>
2024-02-14 15:03:03 +0530

    Added the algolia search only api key

--- server/algolia.ts
+++ server/algolia.ts
@@ -5,16 +5,16 @@ import { env } from "~/env";
 import { Record } from "~/types/records";
 
 export async function search(key: string): Promise<Record[]> {
-  const client = algoliasearch(env.ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
-  const index = client.initIndex(env.ALGOLIA_INDEX_NAME);
+  const client = algoliasearch(env.NEXT_PUBLIC_ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
+  const index = client.initIndex(env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME);
 
   const res = await index.search(key);
   return res.hits as unknown as Record[];
 }
 
 export async function saveRecords(records: Record[]) {
-  const client = algoliasearch(env.ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
-  const index = client.initIndex(env.ALGOLIA_INDEX_NAME);
+  const client = algoliasearch(env.NEXT_PUBLIC_ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
+  const index = client.initIndex(env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME);
 
   await index.saveObjects(records)
 }
