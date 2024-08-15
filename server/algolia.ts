e2034c850f1a2991182e28ff2142db850251665a
bakesbyish <ishani@bakesbyish.com>
2024-08-15 23:19:40 +0530

    feat(algolia): increased the hits per page

--- server/algolia.ts
+++ server/algolia.ts
@@ -9,13 +9,18 @@ export async function search(key: string): Promise<Record[]> {
   const client = algoliasearch(env.ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
   const index = client.initIndex(env.ALGOLIA_INDEX_NAME);
 
-  const res = await index.search(key);
+  const res = await index.search(key, {
+    hitsPerPage: 1000,
+  });
   return res.hits as unknown as Record[];
 }
 
 export async function saveRecords(records: Record[]) {
-  const client = algoliasearch(env.NEXT_PUBLIC_ALGOLIA_ID, env.ALGOLIA_WRITE_KEY);
+  const client = algoliasearch(
+    env.NEXT_PUBLIC_ALGOLIA_ID,
+    env.ALGOLIA_WRITE_KEY,
+  );
   const index = client.initIndex(env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME);
 
-  await index.saveObjects(records)
+  await index.saveObjects(records);
 }
